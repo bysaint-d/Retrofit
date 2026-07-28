ded4a10da8b6eaee3d2088aefb09391b6239bda6
Elmir Shaliyev <elmirshaliyev2007@gmail.com>
2026-07-29 01:19:55 +0400

    Initial commit

---
+++ app/src/main/java/com/shaliyev/rerofitgradlegroovy/view/MainActivity.java
@@ -0,0 +1,93 @@
+package com.shaliyev.rerofitgradlegroovy.view;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.shaliyev.rerofitgradlegroovy.R;
+import com.shaliyev.rerofitgradlegroovy.model.CryptoModel;
+import com.shaliyev.rerofitgradlegroovy.service.CryptoAPI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<CryptoModel> cryptoModels;
+    private String Base_URL = "https://raw.githubusercontent.com/";
+    Retrofit retrofit;
+    RecyclerView recyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        recyclerView =findViewById(R.id.recycleview);
+
+
+        Gson gson = new GsonBuilder().setLenient().create();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(Base_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        loadData();
+    }
+
+    private void loadData() {
+
+        CryptoAPI cryptoAPI = retrofit.create(CryptoAPI.class);
+
+        Call<List<CryptoModel>> call = cryptoAPI.getData();
+
+        call.enqueue(new Callback<List<CryptoModel>>() {
+
+            @Override
+            public void onResponse(Call<List<CryptoModel>> call, Response<List<CryptoModel>> response) {
+
+                if (response.isSuccessful()) {
+
+                    List<CryptoModel> responseList = response.body();
+                    cryptoModels = new ArrayList<>(responseList);
+
+                    for (CryptoModel crypto : responseList) {
+                        System.out.println(crypto.currency);
+                        System.out.println(crypto.price);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<CryptoModel>> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
+
+    }
+
+
+
+}
